9a2bda65f93febdcfe585db9a34abfd61eb73b01
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-13 23:46:34 +0500

    update

--- index.php
+++ index.php
@@ -1,36 +1,8 @@
 <?php
-
-// String
-// Butun son
-// Float (suzuvchi nuqta raqamlari - ikki marta ham deyiladi)
-// Mantiqiy
-// Massiv
-// Ob'ekt
-// NULL
-// Resurs
-$string = "javohir";
-$ingater = 23;
-$float = 34.5;
-$boolean = true | false;
-$newArray = [1, 2, 3  , "salom dunyo"];
-// print_r($newArray);
-
-class Car{
-    public $model;
-    public $color;
-
-    public function __construct($model, $color){
-        $this->color = $color;
-        $this->model = $model;
-    }
-
-    public function message(){
-        return 'my car model ' . $this->model . ", " . "my car's rangi" . " ". $this->color;
-    }   
-}
-
-$myCar = new Car('nexia' , "mokri");
-
-echo $myCar -> message()
-
+$gap = 'salom mening ismim javohir';
+// echo strpos($gap , "ismim")
+// echo strrev($gap)
+// echo str_word_count($gap);
+// echo str_replace("salom" , "assalom aleykum" , $gap);
+// echo strlen($gap)
 ?>
