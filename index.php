43531310f76de724ddae3ec61767140e1bbbf88d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-15 13:15:53 +0500

    update

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-$gap = 'salom mening ismim javohir';
-// echo strpos($gap , "ismim")
-// echo strrev($gap)
-// echo str_word_count($gap);
-// echo str_replace("salom" , "assalom aleykum" , $gap);
-// echo strlen($gap)
+$x=5;
+$y= $x++;
+echo $y;
+// echo $y;
 ?>
