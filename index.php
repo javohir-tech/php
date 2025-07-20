2aae5099c1e0bdd5ae6ac7db85cb867a0854a7f2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-21 00:00:37 +0500

    update

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
 
-$ism = "javohir";
-$age = 21;
+/* 
+string
+*/
 
-echo "<h2> " . $ism . " </h2>";
+$string = "Javohir"; // matn
+$integer =  21 ; // butun son
+$float = 62.5; // butun bo'lmagan son
+$boolean =  true ; // true || false
+$arr =  ["javohir" , 21 , 64.5 , false];
+$n = null;
 
-print false
-    ?>
+class MyCar {
+    public $car;
+    public $color ;
+
+    public function __construct($car , $color){
+        $this->car =  $car;
+        $this->color =  $color;
+    }
+
+    public function message(){
+        return $this->car . " color is " . $this->color;
+    }
+}
+
+$message =  new MyCar("nexia" ,  "mokri");
+echo $message->message()
+
+?>
