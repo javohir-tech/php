d13df5e53512b4bbd4f40b47e6338bb9400c8666
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-22 00:04:18 +0500

    update

--- index.php
+++ index.php
@@ -4,28 +4,38 @@
 string
 */
 
-$string = "Javohir"; // matn
-$integer =  21 ; // butun son
-$float = 62.5; // butun bo'lmagan son
-$boolean =  true ; // true || false
-$arr =  ["javohir" , 21 , 64.5 , false];
-$n = null;
+// $string = "Javohir"; // matn
+// $integer =  21 ; // butun son
+// $float = 62.5; // butun bo'lmagan son
+// $boolean =  true ; // true || false
+// $arr =  ["javohir" , 21 , 64.5 , false];
+// $n = null;
 
-class MyCar {
-    public $car;
-    public $color ;
+// class MyCar {
+//     public $car;
+//     public $color ;
 
-    public function __construct($car , $color){
-        $this->car =  $car;
-        $this->color =  $color;
-    }
+//     public function __construct($car , $color){
+//         $this->car =  $car;
+//         $this->color =  $color;
+//     }
 
-    public function message(){
-        return $this->car . " color is " . $this->color;
-    }
-}
+//     public function message(){
+//         return $this->car . " color is " . $this->color;
+//     }
+// }
 
-$message =  new MyCar("nexia" ,  "mokri");
-echo $message->message()
+// $message =  new MyCar("nexia" ,  "mokri");
+// echo $message->message()
+
+
+$gap = "salom mening ismim Javohir";
+
+// echo strlen($gap)
+// echo str_replace("salom" ,  "Assalom Aleykum " , $gap) // matn ichidagi so'zni boshqa so'zga almashtiradi 
+// echo strrev($gap)
+// echo  str_word_count($gap)
+
+
 
 ?>
