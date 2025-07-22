ed3a73e24243ba4f5d6627d376cfa884aff8c4c1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-22 23:41:55 +0500

    update

--- index.php
+++ index.php
@@ -1,54 +1,9 @@
 <?php
+// php da constantalar define() funksiyasi bilan yaratiladi 
+// birinchi qiymatga constantani nomi , odatada katta harfda 
+// ikkinchi qiymat constantaga biriktiriladigan qiymay any || void
+define("Name"  , "Javohir");
+define("age"  , 21);
 
-/* 
-string
-*/
-
-// $string = "Javohir"; // matn
-// $integer =  21 ; // butun son
-// $float = 62.5; // butun bo'lmagan son
-// $boolean =  true ; // true || false
-// $arr =  ["javohir" , 21 , 64.5 , false];
-// $n = null;
-
-// class MyCar {
-//     public $car;
-//     public $color ;
-
-//     public function __construct($car , $color){
-//         $this->car =  $car;
-//         $this->color =  $color;
-//     }
-
-//     public function message(){
-//         return $this->car . " color is " . $this->color;
-//     }
-// }
-
-// $message =  new MyCar("nexia" ,  "mokri");
-// echo $message->message()
-
-$pi = pi() ;
-
-// echo $pi;
-
-$min = min(1, 2, 3, -2);
-
-// echo $min
-
-$max = max(32 , 43);
-
-// echo $max
-
-$abs =  abs(-2);
-
-// echo $abs
-
-$rand =  rand();
-
-// echo $rand
-
-$round = round(3.6);
-
-// echo $round
+echo Name . "<br>" . age
 ?>
