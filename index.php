8e499efb866aeffdcc8a82d2a9c575616b1b0126
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-23 23:31:11 +0500

    update

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-// php da constantalar define() funksiyasi bilan yaratiladi 
-// birinchi qiymatga constantani nomi , odatada katta harfda 
-// ikkinchi qiymat constantaga biriktiriladigan qiymay any || void
-define("Name"  , "Javohir");
-define("age"  , 21);
-
-echo Name . "<br>" . age
+$x = 10;
+$y = 9;
+$y=$x;
+echo $y
 ?>
