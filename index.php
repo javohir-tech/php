20b736abec8f289cdf50da8b08993893521bde06
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-25 00:31:54 +0500

    update

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
 // taqoslash operatorlari  || comparison operators 
 // oshirish kamaytrish operatorlari ||  Increment/Decrement operators
 
+$x= 5;
+$y = $x++;
+echo $y;
+echo "<br>";
+echo $x;
 
 ?>
