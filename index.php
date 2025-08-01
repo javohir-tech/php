2ed4437083d36ce830a8d0c1d8015690f6bbf28c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-01 22:39:20 +0500

    update

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <?php declare(strict_types=1);
+$ozgaruvchi = "salom dunyo";
 
-// matemetik operatorlar  || arifmetik operators
-// belgilash operatorlar || assigment operators
-// taqoslash operatorlari  || comparison operators 
-// oshirish kamaytrish operatorlari ||  Increment/Decrement operators
-// logika operatorlari || logical operators
-// string operatorlari || string operators
-// massiv operatorlari || array operators
-// + ikkita massivni qo'shadi 
-// == ikkita massivni bir hil kalit-qiymatlari borligini tekshiradi tartip muhim emas 
-// === ikkita masivni kalit-qiymati va data type bir hil bo'lishi kerak , tartip muhim 
+echo "oldin" . "<br>";
 
-function myFunction(int $a, int $b) : int
-{
-return $a+$b;
-}
+// var_dump($ozgaruvchi);
+die($ozgaruvchi);
 
-$res = myFunction(5 ,   10);
-echo $res;
+echo " <br> keyin" ;
 ?>
