dd8e63b62a75d3fd45d92bd7550a9171a7fc6348
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-03 22:51:39 +0500

    update

--- index.php
+++ index.php
@@ -4,28 +4,17 @@
 // Associative arrays - Arrays with named keys || indexlari kalit so'zlardan iborat array
 // Multidimensional arrays - Arrays containing one or more arrays || array ichida array bolsa
 
-//indexed Arrays
-$indexedArr = [12, "salom", true, 1.2];
+//Super globals 
+// $GLOBALS
+// $_SERVER
+// $_REQUEST
+// $_POST
+// $_GET
+// $_FILES
+// $_ENV
+// $_COOKIE
+// $_SESSION
 
-// foreach(indexedArr as $item){
-//     echo $item ."<br>";
-// }
-
-//Associative Arrays
-
-$AssArr = ["javohir" => 21, "ulug'bek" => 23];
-
-// foreach ($AssArr as $key => $value) {
-//     echo "$key : $value <br>";
-// }
-
-// Multidimensional Array;
-
-$MultiArr = [
-   "odamlar" => ["javohir" => 21 , "Ulug'bek" => 23],
-   "new" => [13 , "hello" , true , 1.2]
-];
-
-echo $MultiArr["new"][1]
+var_dump($_SERVER["PHP_SELF"])
 
 ?>
