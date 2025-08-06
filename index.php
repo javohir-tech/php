cc3b2815ec88ad0bd11c8d8cb5c88a3ea874394b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-06 23:58:06 +0500

    updaate

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
-
+// var_dump($_SERVER)/
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $sur_name = $_POST['sur_name'];
+}
 
 ?>
 
@@ -12,10 +16,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
     <style>
-        .form-box { 
-            display :flex;
+        .form-box {
+            display: flex;
             gap: 10px;
-            justify-content :center;
+            justify-content: center;
             margin-top: 10px;
         }
     </style>
@@ -23,7 +27,7 @@
 
 <body>
     <h1 style="text-align : center">Forma</h1>
-    <form action="form.php" method="POSt">
+    <form action="" method="POSt">
         <div class="form-box">
             <label for="name">Ismingiz</label>
             <input type="text" id="name" name="name">
@@ -36,6 +40,10 @@
             <button>Jo'natish</button>
         </div>
     </form>
+    <div>
+        <h1>Ismingiz : <?= $name ?? "" ?></h1>
+        <h1>Familyangiz : <?= $sur_name ?? "" ?></h1>
+    </div>
 </body>
 
 </html>
